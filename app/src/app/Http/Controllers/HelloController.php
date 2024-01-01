ca377dc476a99f2c7c6712d0c50b1ddde05b17ca
Tsugumi Sato <thrush1105@gmail.com>
2024-01-01 19:50:10 +0900

    HelloControllerでログ出力しないように修正

--- app/src/app/Http/Controllers/HelloController.php
+++ app/src/app/Http/Controllers/HelloController.php
@@ -2,13 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Log;
-
 class HelloController
 {
     public function hello()
     {
-        Log::debug("Hello!!");
         return response("Hello!!!");
     }
 }
